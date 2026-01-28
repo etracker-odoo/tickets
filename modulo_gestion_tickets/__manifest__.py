b71607d0f837120ccac44cdca9e894fe9357be43
E-Tracker <sistemas@e-tracker.com.mx>
2026-01-27 18:55:28 -0600

    Update __manifest__.py

--- modulo_gestion_tickets/__manifest__.py
+++ modulo_gestion_tickets/__manifest__.py
@@ -1,13 +1,1 @@
-{
-    'name': 'Etracker Ticket System',
-    'version': '1.0',
-    'category': 'Services',
-    'summary': 'Gestión de tickets interna para Etracker',
-    'depends': ['base'],
-    'data': [
-        'views/ticket_views.xml',
-    ],
-    'installable': True,
-    'application': True,
-    'license': 'LGPL-3',
-}
+
