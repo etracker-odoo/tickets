ad494738ebd687c00eb43f046a9fb16cc98bbd7f
E-Tracker <sistemas@e-tracker.com.mx>
2026-01-27 18:19:56 -0600

    Create __manifest__.py

---
+++ modulo_gestion_tickets/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Gestor de Tickets Personalizado',
+    'version': '1.0',
+    'summary': 'Sistema para administrar reportes',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+        'views/tickets_principal_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
